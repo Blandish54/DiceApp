9fc34f714f74641dc1f9eb952259fd5dfb5f6df8
Corey Bland <blandcorey92@gmail.com>
2024-12-03 16:54:52 -0600

    cleaned up code and added ability to select die size

--- DiceApp/Program.cs
+++ DiceApp/Program.cs
@@ -5,43 +5,34 @@ using System.Reflection.Metadata.Ecma335;
 //As a user of this dice app
 //I want the ability to say how many sides my die has
 //so I can roll different sized dice
-
+bool rollDiceAgain = true;
 Random randomDiceGenerator = new Random();
 
-Console.WriteLine  ("Hello and Welcome to The Dice App");
-
-Console.WriteLine  ("Please enter the number of sides you would like your die to have");
-    
-Console.WriteLine  ("Enter a number between 2 and 100");
-
-string userInput = Console.ReadLine();
-
-bool howManySides = false;
-
-switch (howManySides)
-{ 
-
-    howManySides = true;
-    
-    int rollValue = randomDiceGenerator.Next(1,2);
-   
-    Console.WriteLine($"You Rolled a: {rollValue} ");}
-  
-
-  
-else { 
-    howManySides = false;}
-
-  
-   
-bool rollDiceAgain = true;
-
-Console.WriteLine("Would you like to roll again?");
-
-Console.WriteLine("Enter Y/N");
+Console.WriteLine("Hello and Welcome to The Dice App");
+Console.WriteLine("Please enter the number of sides you would like your die to have");
 
 while (rollDiceAgain)
 {
+    Console.WriteLine("Enter a number between 2 and 100");
+
+    string howManySides = Console.ReadLine();
+
+    int sidesConverted;
+
+    bool isUserInputOk = Int32.TryParse(howManySides, out sidesConverted);
+    if (isUserInputOk == false)
+    {
+        Console.WriteLine("It seems you have entered an incorrect input.");
+
+        continue;
+    }
+    int rollValue = randomDiceGenerator.Next(1, sidesConverted);
+
+    Console.WriteLine($"You Rolled a: {rollValue} ");
+    Console.WriteLine("Would you like to roll again?");
+    Console.WriteLine("Enter Y/N");
+
+    string userInput = Console.ReadLine();
 
     if (userInput == "y" || userInput == "Y")
     {
