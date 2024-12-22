da70346cdfbf3d1e0cca87b118edca757c84ff93
Corey Bland <blandcorey92@gmail.com>
2024-12-21 22:49:49 -0600

    wip: temporary check in

--- DiceApp/Program.cs
+++ DiceApp/Program.cs
@@ -1,11 +1,7 @@
 ﻿using System.ComponentModel.Design;
 using System.Net.Security;
 using System.Reflection.Metadata.Ecma335;
-
-//TIP: look at our while loop example
-//As a user of this dice app
-//I want the ability to also say how many dice I should roll 
-//so I can roll multiple of the same die and get the sum of their values
+using System.Text.Json;
 
 //As a user of this dice app
 //I want the ability to see the value of each die I rolled along with the sum total
@@ -15,29 +11,103 @@ bool rollDiceAgain = true;
 Random randomDiceGenerator = new Random();
 
 Console.WriteLine("Hello and Welcome to The Dice App");
-Console.WriteLine("Please enter the number of sides you would like your die to have");
-
-
-
 
 while (rollDiceAgain)
 {
-    Console.WriteLine("Enter a number between 2 and 100");
+    int diceSelection = 0;
+    bool isSidesANumber = false;
+    bool isFirstSidesLoop = true;
 
-    string howManySides = Console.ReadLine();
-
-    int sidesConverted;
-
-    bool isUserInputOk = Int32.TryParse(howManySides, out sidesConverted);
-    if (isUserInputOk == false)
+    while (isSidesANumber == false)
     {
-        Console.WriteLine("It seems you have entered an incorrect input.");
+        if (isFirstSidesLoop)
+        {
+            Console.WriteLine("First we need to select what kind of die you would like to roll");
+            Console.WriteLine("Enter a: 1 to flip a coin");
+            Console.WriteLine("Enter a: 2 to roll a standard 6-sided die");
+            Console.WriteLine("Enter a: 3 to roll a 10-sided die");
+            Console.WriteLine("Enter a: 4 to roll a 20-sided die");
+            Console.WriteLine("Enter a: 5 to roll a 100-sided die");
+            
+            Console.ReadLine();
+            
+            isFirstSidesLoop = false;
+        }
+       else
+        {
+         Console.WriteLine("Enter the number of times you would like to roll your selected die or coin.");
+        }
+        string howManySides = Console.ReadLine();
 
-        continue;
+        isSidesANumber = Int32.TryParse(howManySides, out diceSelection);
+        
+        
     }
-    int rollValue = randomDiceGenerator.Next(1, sidesConverted);
+    int numberOfDice = 0;
+    bool isDiceInputOk = false;
+    bool isFirstDiceLoop = true;
 
-    Console.WriteLine($"You Rolled a: {rollValue} ");
+    while (isDiceInputOk == false)
+    {
+
+        if (isFirstDiceLoop)
+        {
+            Console.WriteLine("Also, enter the number of dice you would like to roll.");
+            isFirstDiceLoop = false;
+        }
+        else
+        {
+            Console.WriteLine("It looks like you failed to enter a number.");
+            Console.WriteLine("Enter a number to continue.");
+        }
+
+        string howManyDice = Console.ReadLine();
+
+
+        isDiceInputOk = Int32.TryParse(howManyDice, out numberOfDice);
+     }
+    switch (diceSelection)
+    {
+        case 0:
+            = randomDiceGenerator.Next(1, 2);
+            break;
+        case 1:
+            int standardDie = randomDiceGenerator.Next(1, 6);
+            break;
+        case 2:
+            int d10 = randomDiceGenerator.Next(1, 10);
+            break;
+        case 3:
+            int d20 = randomDiceGenerator.Next(1, 20);
+            break;
+        case 4:
+            int d100 = randomDiceGenerator.Next(1, 100);
+            break;
+        default:
+            
+            Console.WriteLine("It seems you have entered an incorrect input.");
+            Console.WriteLine("Please select an input from the list above");
+
+            Console.ReadLine();
+            
+            
+        }       
+    
+
+    int rollValues = 0;
+    int sum = 0;
+    for (int i = 1; i <= numberOfDice ; i++)
+    {
+        rollValues = randomDiceGenerator.Next(1, diceSelection);
+
+        Console.WriteLine(rollValues);
+
+        sum += rollValues;
+    }
+
+     
+
+    Console.WriteLine($"You Rolled a total of: {sum} ");
     Console.WriteLine("Would you like to roll again?");
     Console.WriteLine("Enter Y/N");
 
