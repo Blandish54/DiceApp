09170045f79d54c9b7ce84891271f288f9153c72
Corey Bland <blandcorey92@gmail.com>
2025-01-14 17:16:48 -0600

    Fixed the bug that kept the second user input for both loops Made the "bug" that allowed the user to enter a decimal when asked for a whole number into a feature Kept the tipDisplay in twice because I need the convert and math.round to show only 2

---
+++ AnimalFarm/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using System;
+using AnimalFarm.Models;
+
+Mammal firstMammal = new Mammal();
+
+Mammal secondMammal = new Mammal("King Louie");
+
+Console.WriteLine($"First Mammal's hair color is:{firstMammal.name}");
--- TipCalculator/Program.cs
+++ TipCalculator/Program.cs
@@ -1,73 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Reflection.Metadata.Ecma335;
+﻿using System;
+using System.Text;
 
-double billTotal = 0;
-bool isUserBillANumber = true;
-bool isUserPercentageANumber = true;
-bool isAnotherBillToTotal = true;
 
-Console.WriteLine("Thank you for opening the Tip Calculator App");
-Console.WriteLine("Enter the total of your bill to continue");
-string userTotal = Console.ReadLine();
-
-while (isAnotherBillToTotal)
+namespace TipCalculator
 {
-    double tipDisplay = 0;
-    double wholeTotal = 0;
-    decimal tipTotal = 0;
-    decimal tipPercentage = 0;
-
-    isUserBillANumber = double.TryParse(userTotal, out billTotal);
-
-    if (isUserBillANumber == false)
+    class Program
     {
-        Console.WriteLine("Please enter a number to continue");
-        userTotal = Console.ReadLine();
-    }
-    else
-    {
-        isUserBillANumber = true;
-    }
+        static void Main(string[] args)
+        {
+            decimal billTotal = 0;
+            bool isUserBillANumber = true;
+            bool isUserPercentageANumber = true;
+            bool isAnotherBillToTotal = true;
+            
+            Console.WriteLine("Thank you for opening the Tip Calculator App");
 
-    Console.WriteLine("Please enter a whole number for the Tip Percentage.");
-    string userPercentage = Console.ReadLine();
+            Console.WriteLine("Enter the total of your bill to continue");
+            string userTotal = Console.ReadLine();
 
-    isUserPercentageANumber = decimal.TryParse(userPercentage, out tipPercentage);
+            while (isAnotherBillToTotal)
+            {
+                decimal wholeTotal = 0;
+                decimal tipTotal = 0;
+                decimal tipPercentage = 0;
 
-    if (isUserPercentageANumber == false)
-    {
-        Console.WriteLine("Please enter a number to continue");
-        userPercentage = Console.ReadLine();
-    }
-    else
-    {
-        isUserPercentageANumber = true;
-    }
-    decimal decimalBillTotal = Convert.ToDecimal(billTotal);
+                isUserBillANumber = decimal.TryParse(userTotal, out billTotal);
 
-    billTotal = Math.Round(billTotal, 2);
+                if (isUserBillANumber == false)
+                {
+                    Console.WriteLine("Please enter a number to continue");
+                    userTotal = Console.ReadLine();
+                    continue;
+                }
 
-    tipTotal = (decimalBillTotal * (tipPercentage / 100));
 
-    tipDisplay = Convert.ToDouble(tipTotal);
 
-    tipDisplay = Math.Round(tipDisplay, 2);
+                Console.WriteLine("Please enter the percentage you would like to tip.");
+                string userPercentage = Console.ReadLine();
 
-    wholeTotal = tipDisplay + billTotal;
+                isUserPercentageANumber = decimal.TryParse(userPercentage, out tipPercentage);
 
-    Console.WriteLine($"The total tip should be {tipDisplay} and the bill should equal {wholeTotal}");
-    Console.WriteLine("Did you have another bill you would like to total? If so enter the total now");
-    userTotal = Console.ReadLine();
 
-    isUserBillANumber = double.TryParse(userTotal, out billTotal);
+                while (isUserPercentageANumber == false)
+                {
+                    //Is this needed?
+                    userPercentage = null;
+                    tipPercentage = 0;
 
-    if (isUserBillANumber == false)
-    {
-        Console.WriteLine("Good-Bye<3");
-        break;
-    }
-    else
-    {
-        continue;
+                    Console.WriteLine("Please enter a number as a percentage to continue.");
+                    userPercentage = Console.ReadLine();
+                    isUserPercentageANumber = decimal.TryParse(userPercentage, out tipPercentage);
+
+                    //Is this needed?
+                    if (isUserPercentageANumber == true)
+                    {
+                        continue;
+                    }
+                }
+
+
+                decimal decimalBillTotal = Convert.ToDecimal(billTotal);
+
+                billTotal = Math.Round(billTotal, 2);
+
+                tipTotal = (decimalBillTotal * (tipPercentage / 100));
+
+                tipTotal = Math.Round(tipTotal, 2);
+
+                wholeTotal = tipTotal + billTotal;
+
+
+
+                Console.WriteLine($"The total tip should be ${tipTotal} and the bill should equal ${wholeTotal}");
+                Console.WriteLine("Did you have another bill you would like to total? If so enter the total now");
+                userTotal = Console.ReadLine();
+
+                isUserBillANumber = decimal.TryParse(userTotal, out billTotal);
+
+                if (isUserBillANumber == false)
+                {
+                    Console.WriteLine("Good-Bye<3");
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
     }
 }
